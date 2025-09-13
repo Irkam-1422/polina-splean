cf6073e6a37241ebd4be6e2e2413e1c7c1ca45a1
Dmitry Mormul <macairmormul@MacBook-Air-Dmitry.local>
2025-09-13 19:46:19 +0200

    fixed bgs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,14 @@ import "./Card.scss";
 import { adjustColor } from "../utils/colors";
 import { useDispatch } from "react-redux";
 import { setWords } from "../actionCreators";
+import bg1 from "../assets/bg1.png";
+import bg2 from "../assets/bg2.png";
+import bg3 from "../assets/bg3.png";
+import bg4 from "../assets/bg4.png";
+import bg5 from "../assets/bg5.png";
+import bg6 from "../assets/bg6.png";
+
+const bgs = [bg1, bg2, bg3, bg4, bg5, bg6];
 
 export const Card = ({
   i = 1,
@@ -60,7 +68,7 @@ export const Card = ({
         <div
           className="inner-div"
           style={{
-            background: `url("src/assets/bg${i}.png")`,
+            background: `url(${bgs[i - 1]})`,
             transform: !open
               ? "none"
               : type === "vertical"
